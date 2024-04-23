6b7e8003611f8a95d6e9d54b1d36dc44787e05a9
Gennaro Raiola <gennaro.raiola@gmail.com>
2024-04-23 11:17:25 +0200

    Remove static transforms

--- tf_throttle/src/tf_throttle.cpp
+++ tf_throttle/src/tf_throttle.cpp
@@ -14,62 +14,66 @@ ros::Publisher pub;
 
 void sendTransforms()
 {
-	std::vector<std::string> frames;
-	g_tf->getFrameStrings(frames);
+  std::vector<std::string> frames;
+  g_tf->getFrameStrings(frames);
 
-	tf2_msgs::TFMessage msg;
-	msg.transforms.reserve(frames.size());
+  tf2_msgs::TFMessage msg;
+  msg.transforms.reserve(frames.size());
 
-	ros::Time now = ros::Time::now();
+  ros::Time now = ros::Time::now();
 
-	BOOST_FOREACH(std::string frame, frames)
-	{
-		std::string parentFrame;
-		if(!g_tf->getParent(frame, ros::Time(0), parentFrame))
-			continue;
+  BOOST_FOREACH(std::string frame, frames)
+  {
+    std::string parentFrame;
+    if(!g_tf->getParent(frame, ros::Time(0), parentFrame))
+      continue;
 
-		tf::StampedTransform transform;
-		try
-		{
-			g_tf->lookupTransform(
-				parentFrame,
-				frame,
-				ros::Time(0),
-				transform
-			);
-		}
-		catch(tf::TransformException&)
-		{
-			continue;
-		}
+    tf::StampedTransform transform;
+    try
+    {
+      g_tf->lookupTransform(
+        parentFrame,
+        frame,
+        ros::Time(0),
+        transform
+      );
+    }
+    catch(tf::TransformException&)
+    {
+      continue;
+    }
 
-		geometry_msgs::TransformStamped m;
-		tf::transformStampedTFToMsg(transform, m);
+    // Remove static transforms
+    if(transform.stamp_.sec <= 0.0)
+      continue;
 
-		msg.transforms.push_back(m);
-	}
+    geometry_msgs::TransformStamped m;
+    tf::transformStampedTFToMsg(transform, m);
 
-	pub.publish(msg);
+    msg.transforms.push_back(m);
+  }
+
+  pub.publish(msg);
 }
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "tf_throttle");
+  ros::init(argc, argv, "tf_throttle");
 
-	ros::NodeHandle nh("~");
+  ros::NodeHandle nh("~");
 
-	g_tf.reset(new tf::TransformListener(nh, ros::Duration(10.0)));
+  g_tf.reset(new tf::TransformListener(nh, ros::Duration(10.0)));
 
-	double rate;
-	nh.param("rate", rate, 4.0);
+  double rate;
+  nh.param("rate", rate, 4.0);
 
-	ros::Timer timer = nh.createTimer(
-		ros::Duration(1.0 / rate),
-		boost::bind(&sendTransforms)
-	);
-	pub = nh.advertise<tf2_msgs::TFMessage>("tf", 1);
+  ros::Timer timer = nh.createTimer(
+    ros::Duration(1.0 / rate),
+    boost::bind(&sendTransforms)
+  );
+  pub = nh.advertise<tf2_msgs::TFMessage>("tf", 1);
 
-	ros::spin();
+  ros::spin();
 
-	return 0;
+  return 0;
 }
